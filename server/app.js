0e4088ecdd6339e3449cf2e7b52ca4f8e28b4e62
aubreytran <aubreyoliviatran@gmail.com>
2026-08-22 22:07:01 -0700

    feat: implement server setup with session management, add initial route files and landing/login HTML

--- server/app.js
+++ server/app.js
@@ -1,1 +1,29 @@
 require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+const app = express();
+
+app.use(
+    session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // expires in 24 hours
+    },
+    resave: true,
+    saveUninitialized: false,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// health check endpoint
+app.get('/health', (_, res) => {
+    res.json({ status: 'ok' });
+});
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/', authRoutes);
+
+module.exports = app;
