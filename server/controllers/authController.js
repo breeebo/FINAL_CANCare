500b90f40ed9b5a9085478c29bcacebf05b8e9e0
aubreytran <aubreyoliviatran@gmail.com>
2026-08-25 17:05:24 -0700

    feat: enhance signup functionality with validation and error handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,53 @@
+const bcrypt = require('bcryptjs');
+const { query } = require("../../database/db.js");
+
 exports.checkSession = (req, res) => {
     if (!req.session.userId) {
         return res.status(401).json({ loggedIn: false });
     }
     res.json({ loggedIn: true, userId: req.session.userId });
+};
+
+
+exports.signup = async (req, res) => {
+    
+    try {
+        const user = req.body
+        const {email, password, name, surname, phone, user_type, location, price_range, specialties} = user
+        const errors = [];
+
+        if (!email) errors.push('Email is required');
+        if (!password) errors.push('Password is required');
+        if (!name) errors.push('First name is required');
+        if (!surname) errors.push('Surname is required');
+        if (!user_type) errors.push('User type is required');
+        if (!location) errors.push('Location is required');
+        if (!price_range) errors.push('Price range is invalid');
+
+        if (errors.length > 0) return res.status(400).json({ errors });
+
+
+        const result = await query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (result.rowCount > 0) {
+            return res.status(400).json({ errors: ['An account with this email already exists'] });
+        }
+
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+        const newResult = await query(
+            'INSERT INTO users (email, password, name, surname, phone, user_type, location, price_range, specialties) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+            [email, hashedPassword, name, surname, phone, user_type, location, price_range, specialties]
+        );
+
+        const userId = newResult.rows[0].id;
+        req.session.userId = userId;
+        res.json({ userId: req.session.userId });
+        
+    } catch (err) {
+        console.error('Signup error:', err);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 };
